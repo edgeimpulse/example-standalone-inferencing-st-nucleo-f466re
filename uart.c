96b8d4d4d8f8a7ee739010c5782fe1e3c2979ab2
francovaro <francovaro@gmail.com>
2023-12-15 16:14:32 +0100

    cleaning stuff

--- uart.c
+++ uart.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2023 EdgeImpulse Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+ 
 #include "uart.h"
 #include "stm32f4xx.h"
 #include "stm32f4xx_hal_uart.h"
@@ -53,9 +70,7 @@ void USART2_UART_Init(void)
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
     uint8_t rx_byte;
-    if (huart->Instance == USART2)
-    {
-        /* Riceve un byte*/
+    if (huart->Instance == USART2) {
         HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
         rx_buffer[rx_idx++] = rx_byte;
     }
